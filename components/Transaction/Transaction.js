61a8149b46de1578276fe5b328dd152803839a22
NguyenThiTuyetBang <bangtuyet2269@gmail.com>
2022-10-03 14:57:26 +0700

    Finish list user

--- components/Transaction/Transaction.js
+++ components/Transaction/Transaction.js
@@ -2,6 +2,7 @@ import styles from "./styles.js";
 import { Text, View, Animated, Image } from "react-native";
 import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
 import images from "../../constants/images";
+import Svg, { Path } from "react-native-svg";
 
 const Transaction = () => {
   const listUser = [
@@ -11,6 +12,7 @@ const Transaction = () => {
       timer: "3 hours ago",
       nft: "-850 USDT",
       status: "decrease",
+      color: null,
     },
     {
       id: 2,
@@ -18,6 +20,7 @@ const Transaction = () => {
       timer: "3 hours ago",
       nft: "+850 MCOIN",
       status: "increase",
+      color: "#E3F7DF",
     },
     {
       id: 3,
@@ -25,6 +28,7 @@ const Transaction = () => {
       timer: "3 hours ago",
       nft: "-80 MCOIN",
       status: "decrease",
+      color: "#FBBCD5",
     },
     {
       id: 4,
@@ -32,6 +36,7 @@ const Transaction = () => {
       timer: "3 hours ago",
       nft: "-850 USDT",
       status: "decrease",
+      color: null,
     },
     {
       id: 5,
@@ -39,6 +44,7 @@ const Transaction = () => {
       timer: "3 hours ago",
       nft: "-850 USDT",
       status: "decrease",
+      color: null,
     },
   ];
 
@@ -67,14 +73,46 @@ const Transaction = () => {
               flexDirection: "row",
             }}
           >
-            <Image
-              style={{
-                height: 50,
-                width: 50,
-                borderRadius: 16,
-              }}
-              source={images.avatar_2}
-            />
+            {item.color !== null ? (
+              <View
+                style={{
+                  height: 50,
+                  width: 50,
+                  backgroundColor: item.color,
+                  borderRadius: 16,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-2 h-2"
+                  style={{
+                    height: 20,
+                    width: 20,
+                  }}
+                >
+                  <Path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                  />
+                </Svg>
+              </View>
+            ) : (
+              <Image
+                style={{
+                  height: 50,
+                  width: 50,
+                  borderRadius: 16,
+                }}
+                source={images.avatar_2}
+              />
+            )}
             <View
               style={{
                 marginLeft: 10,
