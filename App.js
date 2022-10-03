cd3a86458e4aa450ab933f18f5cc8d9a60552eb2
NguyenThiTuyetBang <bangtuyet2269@gmail.com>
2022-10-03 09:51:57 +0700

    Modify array

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import data from "./data.json";
 
 export default function App() {
+  const [currentData, setCurrentData] = useState([]);
+  const filterArray = (data) => {
+    let array = [];
+    data.filter((item, index) => {
+      if (data.indexOf(item) === index) {
+        array.push({
+          name: item,
+          index: data.indexOf(item),
+        });
+      }
+    });
+    setCurrentData(array.slice(0, 24));
+  };
+  useEffect(() => {
+    filterArray(data);
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>App!</Text>
+      <Text style={styles.text}>App!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +34,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    color: "red",
+    fontSize: "30px",
   },
 });
